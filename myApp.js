2b0fb2b34d40d4df21c31e1117088ab37bb49752
Simone-Earl <madarang.simone@gmail.com>
2025-10-02 19:39:26 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
 
 
 let Person;
